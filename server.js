cbc914216dfb3d4b9e8e09334331e1264472c19a
alexa-grau <agrau@scu.edu>
2022-03-04 21:08:37 -0800

    Added back personalized for now

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.urlencoded())
+// app.use(express.urlencoded())
 app.use(cors())
 
 app.get('/', function(req, res) {
